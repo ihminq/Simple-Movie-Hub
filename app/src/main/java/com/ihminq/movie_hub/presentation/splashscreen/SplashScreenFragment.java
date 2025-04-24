dc265a7ac5ba0824acd573520d7aa7b22e090e62
hminq <hminh250104@gmail.com>
2025-04-24 17:20:49 +0700

    feat(splashscreen): add SplashScreenFragment & layout file

---
+++ app/src/main/java/com/ihminq/movie_hub/presentation/splashscreen/SplashScreenFragment.java
@@ -0,0 +1,31 @@
+package com.ihminq.movie_hub.presentation.splashscreen;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ihminq.movie_hub.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenFragment extends Fragment {
+
+    public SplashScreenFragment() {}
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash_screen, container, false);
+    }
+}
