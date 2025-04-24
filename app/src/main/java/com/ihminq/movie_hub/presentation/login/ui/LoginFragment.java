9efe19b64a60b350cfc2ea8d4f00f7aa474ccc1b
hminq <hminh250104@gmail.com>
2025-04-24 22:09:02 +0700

    feat(auth): Implement navigation from Login to Register screen

--- app/src/main/java/com/ihminq/movie_hub/presentation/login/ui/LoginFragment.java
+++ app/src/main/java/com/ihminq/movie_hub/presentation/login/ui/LoginFragment.java
@@ -2,27 +2,64 @@ package com.ihminq.movie_hub.presentation.login.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.ihminq.movie_hub.R;
+import com.ihminq.movie_hub.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
+    private FragmentLoginBinding mBinding;
+    private Button mBtnSignIn;
+    private TextView mTvRegister;
+    private EditText mEtEmail, mEtPassword;
+    private NavController mNavController;
 
     public LoginFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //get NavController
+        mNavController = NavHostFragment.findNavController(this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        mBinding = FragmentLoginBinding.inflate(inflater, container, false);
+
+        initViews();
+        initListeners();
+        return mBinding.getRoot();
+    }
+
+    private void initListeners() {
+        registerClickedListener();
+    }
+
+    private void registerClickedListener() {
+        mTvRegister.setOnClickListener(v -> navigateToRegister());
+    }
+
+    private void navigateToRegister() {
+        mNavController.navigate(R.id.action_loginFragment_to_registerFragment);
+    }
+
+    private void initViews() {
+        mBtnSignIn = mBinding.btnLogin;
+        mTvRegister = mBinding.tvRegister;
+        mEtEmail = mBinding.etEmail;
+        mEtPassword = mBinding.etPassword;
     }
 }
