e5b609cf267abf30c8e3dd5021f34cd5bdd5cc42
hminq <hminh250104@gmail.com>
2025-04-24 23:27:19 +0700

    feat(auth): Implement navigation from Register back to Login screen

--- app/src/main/java/com/ihminq/movie_hub/presentation/register/ui/RegisterFragment.java
+++ app/src/main/java/com/ihminq/movie_hub/presentation/register/ui/RegisterFragment.java
@@ -3,14 +3,21 @@ package com.ihminq.movie_hub.presentation.register.ui;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.ihminq.movie_hub.R;
+import android.widget.Button;
+import android.widget.EditText;
+import com.ihminq.movie_hub.databinding.FragmentRegisterBinding;
 
 public class RegisterFragment extends Fragment {
+    private FragmentRegisterBinding mBinding;
+    private NavController mNavController;
+    private Button mBackBtn;
+    private EditText mEtFullname, mEtEmail, mEtPassword;
 
     public RegisterFragment() {}
 
@@ -18,12 +25,35 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //get NavController
+        mNavController = NavHostFragment.findNavController(this);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
+
+        mBinding = FragmentRegisterBinding.inflate(inflater, container, false);
+
+        initViews();
+        initListeners();
+        return mBinding.getRoot();
+    }
+
+    private void initListeners() {
+        mBackBtn.setOnClickListener(v -> navigateBackToLogin());
+    }
+
+    private void navigateBackToLogin() {
+        mNavController.popBackStack();
+    }
+
+    private void initViews() {
+        mBackBtn = mBinding.btnBack;
+        mEtFullname = mBinding.etFullname;
+        mEtEmail = mBinding.etEmail;
+        mEtPassword = mBinding.etPassword;
     }
 }
