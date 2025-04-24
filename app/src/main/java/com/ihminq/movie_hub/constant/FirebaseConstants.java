9d7f83523b7ef709bdc921043a99ee923e3e6607
hminq <hminh250104@gmail.com>
2025-04-24 16:44:28 +0700

    feat: Define core Firebase constants

--- app/src/main/java/com/ihminq/movie_hub/constant/FirebaseConstants.java
+++ app/src/main/java/com/ihminq/movie_hub/constant/FirebaseConstants.java
@@ -1,4 +1,8 @@
 package com.ihminq.movie_hub.constant;
 
+import com.ihminq.movie_hub.BuildConfig;
+
 public class FirebaseConstants {
+    public static final String USERS_REF = "users";
+    public static final String FIREBASE_URL = BuildConfig.FIREBASE_URL;
 }
