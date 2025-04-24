5e4dfda0eca0441aa3297d9eaab1b6d9b3161f6a
hminq <hminh250104@gmail.com>
2025-04-24 17:20:26 +0700

    refactor: rename SplashScreenActivity to MainActivity

---
+++ app/src/main/java/com/ihminq/movie_hub/presentation/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ihminq.movie_hub.presentation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ihminq.movie_hub.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
