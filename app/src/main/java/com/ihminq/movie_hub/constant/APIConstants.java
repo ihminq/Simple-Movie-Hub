adf6fdf7eb89a867233f0b6f176e1e74e01bde82
hminq <hminh250104@gmail.com>
2025-04-24 16:43:55 +0700

    feat: Define core API constants

--- app/src/main/java/com/ihminq/movie_hub/constant/APIConstants.java
+++ app/src/main/java/com/ihminq/movie_hub/constant/APIConstants.java
@@ -1,4 +1,9 @@
 package com.ihminq.movie_hub.constant;
 
+import com.ihminq.movie_hub.BuildConfig;
+
 public class APIConstants {
+    public static final String API_KEY = BuildConfig.API_KEY;
+    public static final String BASE_URL = "api.themoviedb.org/3/movie";
+    public static final String BASE_POSTER_URL = "image.tmdb.org/t/p/";
 }
