032f3db8ffbf132cc838bf89ff7f5d17349d951f
hminq <hminh250104@gmail.com>
2025-04-25 17:25:56 +0700

    feat(auth): Add Adapter for HomeViewPager

---
+++ app/src/main/java/com/ihminq/movie_hub/presentation/home/adapter/HomeViewPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.ihminq.movie_hub.presentation.home.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.ihminq.movie_hub.presentation.movies.FirstTabFragment;
+import com.ihminq.movie_hub.presentation.setting.ThirdTabFragment;
+import com.ihminq.movie_hub.presentation.userprofile.FourthTabFragment;
+import com.ihminq.movie_hub.presentation.watchlist.SecondTabFragment;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class HomeViewPagerAdapter extends FragmentStateAdapter {
+    private final List<Fragment> fragmentList = Arrays.asList(
+            new FirstTabFragment(),
+            new SecondTabFragment(),
+            new ThirdTabFragment(),
+            new FourthTabFragment()
+    );
+
+    public HomeViewPagerAdapter(@NonNull Fragment fragment) {
+        super(fragment);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return fragmentList.size();
+    }
+}
