fd5ad012553b346cad5f1b464ff8eab2ba5357a7
hminq <hminh250104@gmail.com>
2025-04-25 17:21:29 +0700

    feat: Add Fragments & layout file

--- app/src/main/java/com/ihminq/movie_hub/presentation/setting/ThirdTabFragment.java
+++ app/src/main/java/com/ihminq/movie_hub/presentation/setting/ThirdTabFragment.java
@@ -1,4 +1,4 @@
-package com.ihminq.movie_hub.presentation.home;
+package com.ihminq.movie_hub.presentation.setting;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.ihminq.movie_hub.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link ThirdTabFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class ThirdTabFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public HomeFragment() {
+    public ThirdTabFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class HomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment ThirdTabFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static ThirdTabFragment newInstance(String param1, String param2) {
+        ThirdTabFragment fragment = new ThirdTabFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return inflater.inflate(R.layout.fragment_third_tab, container, false);
     }
 }
