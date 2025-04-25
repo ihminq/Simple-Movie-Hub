3b4e292dfd60e1e6ffd80c69e37bde363e5a4377
hminq <hminh250104@gmail.com>
2025-04-25 17:25:19 +0700

    feat(auth): Add RegisterUseCase

---
+++ app/src/main/java/com/ihminq/movie_hub/domain/usecase/auth/RegisterUseCase.java
@@ -0,0 +1,51 @@
+package com.ihminq.movie_hub.domain.usecase.auth;
+
+import android.util.Log;
+
+import com.ihminq.movie_hub.domain.exception.ValidationException;
+import com.ihminq.movie_hub.domain.model.auth.User;
+import com.ihminq.movie_hub.domain.repository.auth.UserRepository;
+import com.ihminq.movie_hub.domain.utils.ValidateResult;
+import com.ihminq.movie_hub.domain.utils.Validator;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import io.reactivex.Completable;
+import io.reactivex.schedulers.Schedulers;
+
+@Singleton
+public class RegisterUseCase {
+    private static final String TAG = "RegisterUseCase";
+    private final UserRepository mUserRepository;
+
+    @Inject
+    public RegisterUseCase(UserRepository userRepository) {
+        this.mUserRepository = userRepository;
+    }
+
+    public Completable execute(User user) {
+        // Validate fullname
+        ValidateResult fullnameResult = Validator.isFullnameValid(user.getFullname());
+        if (!fullnameResult.isValid()) {
+            return Completable.error(new ValidationException(fullnameResult));
+        }
+
+        // Validate email
+        ValidateResult emailResult = Validator.isEmailValid(user.getEmail());
+        if (!emailResult.isValid()) {
+            return Completable.error(new ValidationException(emailResult));
+        }
+
+        // Validate password
+        ValidateResult passwordResult = Validator.isPasswordValid(user.getPassword());
+        if (!passwordResult.isValid()) {
+            return Completable.error(new ValidationException(passwordResult));
+        }
+
+        Log.d(TAG, "User info valid");
+        return mUserRepository.register(user)
+                .subscribeOn(Schedulers.io());
+
+    }
+}
