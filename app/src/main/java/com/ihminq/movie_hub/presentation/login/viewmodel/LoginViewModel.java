1b907429846de979637c2ec39f5d262e32e80bf7
hminq <hminh250104@gmail.com>
2025-04-25 17:28:02 +0700

    feat(auth): Implement LoginUseCase & provide LiveData for UI to observe changes

--- app/src/main/java/com/ihminq/movie_hub/presentation/login/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/ihminq/movie_hub/presentation/login/viewmodel/LoginViewModel.java
@@ -1,6 +1,72 @@
 package com.ihminq.movie_hub.presentation.login.viewmodel;
 
+import android.util.Log;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.ihminq.movie_hub.domain.exception.ValidationException;
+import com.ihminq.movie_hub.domain.model.auth.User;
+import com.ihminq.movie_hub.domain.usecase.auth.LoginUseCase;
+import com.ihminq.movie_hub.domain.utils.ValidateResult;
+import com.ihminq.movie_hub.presentation.utils.SingleEvent;
+
+import javax.inject.Inject;
+
+import dagger.hilt.android.lifecycle.HiltViewModel;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+
+@HiltViewModel
 public class LoginViewModel extends ViewModel {
+    private static final String TAG = "LoginViewModel";
+    private final LoginUseCase mLoginUseCase;
+    private final CompositeDisposable mDisposables = new CompositeDisposable();
+    private final MutableLiveData<SingleEvent<ValidateResult>> mLoginResultLive;
+
+    @Inject
+    public LoginViewModel(LoginUseCase loginUseCase) {
+        this.mLoginUseCase = loginUseCase;
+        mLoginResultLive = new MutableLiveData<>();
+    }
+
+    public void login(User user) {
+        Log.d(TAG, "User to Login: " + user);
+        Disposable disposable = mLoginUseCase.execute(user)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        () -> mLoginResultLive.postValue(new SingleEvent<>(new ValidateResult(true, "Login successful"))),
+                        err -> {
+                            ValidateResult vr = err instanceof ValidationException
+                                    ? ((ValidationException)err).getResult()
+                                    : new ValidateResult(false, err.getMessage(), ValidateResult.Field.NONE);
+                            mLoginResultLive.postValue(new SingleEvent<>(vr));
+                        }
+                );
+        mDisposables.add(disposable);
+    }
+
+    public LiveData<SingleEvent<ValidateResult>> getLoginResultLive() {
+        return this.mLoginResultLive;
+    }
+
+    public void logout() {
+
+    }
+
+    public boolean isCurrentUserLoggedIn() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        return currentUser != null;
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        mDisposables.clear();
+    }
 }
