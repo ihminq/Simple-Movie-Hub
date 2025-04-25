e41e6546617f539a337d95d3eae30b7423e25a22
hminq <hminh250104@gmail.com>
2025-04-25 17:27:12 +0700

    feat: Add constants variables for ResConstants

--- app/src/main/java/com/ihminq/movie_hub/constant/ResConstants.java
+++ app/src/main/java/com/ihminq/movie_hub/constant/ResConstants.java
@@ -1,4 +1,14 @@
 package com.ihminq.movie_hub.constant;
 
+import com.ihminq.movie_hub.R;
+
 public class ResConstants {
+    public static final int IC_TAB_1 = R.drawable.ic_tab_1;
+    public static final int IC_TAB_2 = R.drawable.ic_tab_2;
+    public static final int IC_TAB_3 = R.drawable.ic_tab_3;
+    public static final int IC_TAB_4 = R.drawable.ic_tab_4;
+    public static final int IC_TAB_1_SELECTED = R.drawable.ic_tab_1_selected;
+    public static final int IC_TAB_2_SELECTED = R.drawable.ic_tab_2_selected;
+    public static final int IC_TAB_3_SELECTED = R.drawable.ic_tab_3_selected;
+    public static final int IC_TAB_4_SELECTED = R.drawable.ic_tab_4_selected;
 }
