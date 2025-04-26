6c1275c1ddc8cacea38488d9ac85fc4cb7117029
hminq <hminh250104@gmail.com>
2025-04-27 01:36:52 +0700

    feat: add more APIConstants var

--- app/src/main/java/com/ihminq/movie_hub/constant/APIConstants.java
+++ app/src/main/java/com/ihminq/movie_hub/constant/APIConstants.java
@@ -4,6 +4,10 @@ import com.ihminq.movie_hub.BuildConfig;
 
 public class APIConstants {
     public static final String API_KEY = BuildConfig.API_KEY;
-    public static final String BASE_URL = "api.themoviedb.org/3/movie";
-    public static final String BASE_POSTER_URL = "image.tmdb.org/t/p/";
+    public static final String BASE_URL = "https://api.themoviedb.org/3/";
+    public static final String BASE_POSTER_URL = "https://image.tmdb.org/t/p/original/";
+    public static final String PATH_POPULAR = "popular";
+    public static final String PATH_UPCOMING = "upcoming";
+    public static final String PATH_TOPRATED = "top_rated";
+    public static final String PATH_NOWPLAYING = "now_playing";
 }
