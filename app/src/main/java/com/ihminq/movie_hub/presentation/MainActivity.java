6fd3d47040935cfa84301953e0d04077b02c9159
hminq <hminh250104@gmail.com>
2025-05-09 16:25:23 +0700

    feat: handle logout Navigation (clear backstack & navigate back to Login)

--- app/src/main/java/com/ihminq/movie_hub/presentation/MainActivity.java
+++ app/src/main/java/com/ihminq/movie_hub/presentation/MainActivity.java
@@ -1,19 +1,49 @@
 package com.ihminq.movie_hub.presentation;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.ihminq.movie_hub.R;
+import com.ihminq.movie_hub.presentation.home.viewmodel.HomeViewModel;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
+    private NavController mNavController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get NavController
+        NavHostFragment navHost = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.main_nav_host);
+        mNavController = navHost.getNavController();
+        HomeViewModel mHomeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        mHomeViewModel.getLoggedOutStatusLive().observe(this, isOut -> {
+            if (Boolean.TRUE.equals(isOut)) {
+//                // pop tất cả destinations trả về startDestination (loginFragment)
+//                mNavController.popBackStack(
+//                        /* destinationId = */ mNavController.getGraph().getStartDestinationId(),
+//                        /* inclusive    = */ false
+//                );
+                Log.d("MAIN", "logout");
+                int graphId = mNavController.getGraph().getId();
+                mNavController.clearBackStack(graphId);
+                mNavController.navigate(R.id.loginFragment);
+            }
+            else {
+                Toast.makeText(this, "Logout failed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
