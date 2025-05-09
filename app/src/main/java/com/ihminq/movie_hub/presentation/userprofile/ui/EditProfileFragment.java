40d097e01aa31da271f9fe74c9b9f06a1a8a29fc
hminq <hminh250104@gmail.com>
2025-05-09 16:27:15 +0700

    feat: handle action click back button (navigate back to ProfileFragment)

--- app/src/main/java/com/ihminq/movie_hub/presentation/userprofile/ui/EditProfileFragment.java
+++ app/src/main/java/com/ihminq/movie_hub/presentation/userprofile/ui/EditProfileFragment.java
@@ -2,65 +2,59 @@ package com.ihminq.movie_hub.presentation.userprofile.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.ihminq.movie_hub.R;
+import com.ihminq.movie_hub.databinding.FragmentEditProfileBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EditProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class EditProfileFragment extends Fragment {
+    private FragmentEditProfileBinding mBinding;
+    private NavController mNavController;
+    private Button mBackBtn;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public EditProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EditProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static EditProfileFragment newInstance(String param1, String param2) {
-        EditProfileFragment fragment = new EditProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public EditProfileFragment() {}
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        // get NavController
+        mNavController = NavHostFragment.findNavController(this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        mBinding = FragmentEditProfileBinding.inflate(inflater, container, false);
+
+        initViews();
+        initListeners();
+        return mBinding.getRoot();
+    }
+
+    private void initListeners() {
+        onBtnBackClicked();
+    }
+
+    private void onBtnBackClicked() {
+        mBackBtn.setOnClickListener(v -> navigateBackToProfile());
+    }
+
+    private void navigateBackToProfile() {
+        mNavController.popBackStack();
+    }
+
+    private void initViews() {
+        mBackBtn = mBinding.btnBack;
     }
 }
